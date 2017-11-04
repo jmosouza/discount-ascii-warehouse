dbf74fafe91f725757339f6eb5c641cbfd13d4e0
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-11-04 14:35:10 -0300

    Remove coverage folder to unclutter git

--- app/products/productPropTypesShape.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default {
-  id: PropTypes.string.isRequired,
-  size: PropTypes.number.isRequired,
-  price: PropTypes.number.isRequired,
-  face: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-};
