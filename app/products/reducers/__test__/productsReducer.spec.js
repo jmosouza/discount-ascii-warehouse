a035c0d94e8595ba3c283e88aabea5a59a67f128
João Marcelo Souza <joaomarcelo9393@gmail.com>
2017-11-07 17:06:07 -0300

    Update tests

--- app/products/reducers/__test__/productsReducer.spec.js
+++ app/products/reducers/__test__/productsReducer.spec.js
@@ -1,12 +1,10 @@
 /* global test, expect */
 
 import rootReducer from '../../../redux/rootReducer';
-import { fetchProductsNoMore, fetchProductsSuccess } from '../../actions/fetchProducts';
-import sampleProducts from '../../components/__test__/sampleProducts';
+import { fetchProductsNoMore } from '../../actions/fetchProducts';
 
 [
   { type: 'UNKNOWN ACTION' },
-  fetchProductsSuccess(sampleProducts[0]),
   fetchProductsNoMore(),
 ].forEach((action) => {
   test(`Dispatch ${action.type}`, () => {
